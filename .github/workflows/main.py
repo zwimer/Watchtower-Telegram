beeff26714988e3fda6e016058710693cc9281bb
zwimer <zwimer@gmail.com>
2022-12-29 11:25:53 -0700

    first

---
+++ .github/workflows/main.py
@@ -0,0 +1,45 @@
+from typing import TYPE_CHECKING
+import logging
+import os
+
+from telegram.ext import Application, CommandHandler, MessageHandler, filters
+import requests
+
+if TYPE_CHECKING:
+    from telegram import ForceReply, Update
+    from telegram.ext import ContextTypes
+
+
+t_var: str = "WATCHTOWER_TELEGRAM_TOKEN"
+w_var: str = "WATCHTOWER_HTTP_API_TOKEN"
+
+
+async def do_update(_: "Update", __: "ContextTypes.DEFAULT_TYPE") -> None:
+    # curl -H "Authorization: Bearer mytoken" localhost:8080/v1/update
+    url = "http://localhost:8080/v1/update"
+    data = {"Authorization": f"Bearer {os.environ[w_var]}"}
+    logging.info(f"Making request to {url} with headers={data}")
+    r = requests.get(url, headers=data)
+    logging.info(f"Response: {r.text}")
+    r.raise_for_status()
+
+
+async def help_cmd(update: "Update", context: "ContextTypes.DEFAULT_TYPE") -> None:
+    await update.message.reply_text("/update To update containers")
+
+
+def main() -> None:
+    logging.basicConfig(level=logging.INFO)
+    app = Application.builder().token(os.environ[t_var]).build()
+
+    app.add_handler(CommandHandler("help", help_cmd))
+    app.add_handler(CommandHandler("start", help_cmd))
+    app.add_handler(CommandHandler("update", do_update))
+
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, help_cmd))
+
+    app.run_polling()
+
+
+if __name__ == "__main__":
+    main()
