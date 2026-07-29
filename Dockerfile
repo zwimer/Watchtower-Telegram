8382e74f8ceb6af01341775252dc0968d2a246d7
zwimer <github@zwimer.com>
2026-07-29 15:27:05 -0400

    Update to use URL instead of splitting into components

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python
+FROM python:3.14
 RUN pip install requests python-telegram-bot
 COPY main.py main.py
 ENTRYPOINT ["python3", "/main.py"]
--- main.py
+++ main.py
@@ -11,12 +11,9 @@ if TYPE_CHECKING:
     from telegram import Update
 
 
-# Pass by env instead of CLI for security
-T_TOKEN_VAR: str = "WATCHTOWER_TELEGRAM_TOKEN"
-W_TOKEN_VAR: str = "WATCHTOWER_HTTP_API_TOKEN"
-
-
 class Watchtower:
+
+    __slots__ = ("_lock", "_log", "_token", "_url")
 
     def __init__(self, watchtower_url: str, token: str) -> None:
         self._url: str = f"{watchtower_url}/v1/update"
@@ -26,12 +23,11 @@ class Watchtower:
         """
         curl -H "Authorization: Bearer <token>" <host>/v1/update
         """
-        if not self._url or not self._token:
-            raise RuntimeError("init not called")
+        log = logging.getLogger("main")
         data = {"Authorization": f"Bearer {self._token}"}
-        logging.info("Making request to %s with headers=%s", self._url, data)
-        r = requests.post(self._url, headers=data, timeout=300)
-        logging.debug("Response: %s", r.text)
+        log.info("Making request to %s with headers=%s", self._url, data)
+        r = requests.post(self._url, headers=data, timeout=300)  # noqa: ASYNC210 (blocking is ok)
+        log.debug("Response: %s", r.text)
         r.raise_for_status()
 
 
@@ -40,25 +36,18 @@ async def help_cmd(update: Update, _: ContextTypes.DEFAULT_TYPE) -> None:
 
 
 def main() -> None:
-
+    url_var: str = "WATCHTOWER_URL"
     parser = argparse.ArgumentParser()
-    parser.add_argument("--host", default="localhost", help="The watchtower host")
-    parser.add_argument("--port", default="8080", help="The watchtower port")
     parser.add_argument(
-        "--protocol",
-        default="http",
-        choices=["http", "https"],
-        help="The watchtower protocol",
+        "--url", default="http://localhost:8080", help=f"The watchtower host; overridden by {url_var}"
     )
     parser.add_argument("--log-level", default="WARNING", help="The log level to use")
     ns = parser.parse_args()
 
-    watchtower = Watchtower(
-        f"{ns.protocol}://{ns.host}:{ns.port}", os.environ[W_TOKEN_VAR]
-    )
+    watchtower = Watchtower(os.environ.get(url_var, f"{ns.url}"), os.environ["WATCHTOWER_HTTP_API_TOKEN"])
     logging.basicConfig(level=logging.getLevelName(ns.log_level.upper()))
 
-    app = Application.builder().token(os.environ[T_TOKEN_VAR]).build()
+    app = Application.builder().token(os.environ["WATCHTOWER_TELEGRAM_TOKEN"]).build()
     app.add_handler(CommandHandler("help", help_cmd))
     app.add_handler(CommandHandler("start", help_cmd))
     app.add_handler(CommandHandler("update", watchtower.update))
